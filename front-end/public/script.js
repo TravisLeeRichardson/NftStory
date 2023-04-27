bfb7c952d14532b010228ab4ee2abdd1d5fb5818
Travis <travis@nervos.org>
2023-04-27 05:59:04 -0400

    Added Front End

---
+++ front-end/public/script.js
@@ -0,0 +1,28 @@
+const submitButton = document.getElementById('submit-button');
+const contractAddressInput = document.getElementById('contract-address');
+const nftImage = document.getElementById('nft-image');
+const nftStory = document.querySelector('#feed');
+
+const getStory = () => {
+    const urlSearchParams = new URLSearchParams({
+        contractAddress: contractAddressInput.value
+    });
+    const url = `http://localhost:3000/results?${urlSearchParams}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            nftImage.src = data.image;
+            nftStory.innerHTML = data.story;
+        })
+        .catch(error => console.error(error));
+};
+
+submitButton.addEventListener('click', async () => {
+
+    getStory();
+
+
+});
